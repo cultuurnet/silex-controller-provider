98a08ca5d167513da5310f5364f36163bd0a2a11
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-10-06 11:52:04 +0200

    III-449: Add new Exception

--- src/InvalidCdbXmlException.php
+++ src/InvalidCdbXmlException.php
@@ -10,10 +10,5 @@ namespace CultuurNet\UDB3SilexEntryAPI;
 
 class InvalidCdbXmlException extends \InvalidArgumentException
 {
-    public static function unexpectedNamespace($namespace, $validNamespaces)
-    {
-        return new InvalidCdbXmlException(
-            'Unexpected namespace' . $namespace . ', expected one of: ' . implode(', ', $validNamespaces)
-        );
-    }
+
 }
