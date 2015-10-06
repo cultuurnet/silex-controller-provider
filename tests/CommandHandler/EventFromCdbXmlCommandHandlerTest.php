396a5c46798fbc4c68aa41208aede95f73f94ace
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-10-06 11:37:44 +0200

    III-449: Add EventFromCdbXmlCommandHandlerTest

---
+++ tests/CommandHandler/EventFromCdbXmlCommandHandlerTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 06/10/15
+ * Time: 10:49
+ */
+namespace CultuurNet\UDB3SilexEntryAPI\CommandHandler;
+
+use PHPUnit_Framework_TestCase;
+
+class EventFromCdbXmlCommandHandlerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EventFromCdbXmlCommandHandler
+     */
+    protected $eventFromCdbXmlCommandHandler;
+
+    public function setUp()
+    {
+        $this->eventFromCdbXmlCommandHandler = new EventFromCdbXmlCommandHandler();
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_the_xml_namespace()
+    {
+        $xml = new \CultuurNet\UDB3\XmlString(file_get_contents(__DIR__ . '/InvalidNamespace.xml'));
+        $addEventFromCdbXml = new \CultuurNet\UDB3SilexEntryAPI\Event\Commands\AddEventFromCdbXml($xml);
+
+        $this->setExpectedException(\CultuurNet\UDB3SilexEntryAPI\InvalidCdbXmlException::class);
+
+        $this->eventFromCdbXmlCommandHandler->handle($addEventFromCdbXml);
+    }
+}
